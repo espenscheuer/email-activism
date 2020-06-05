b8025952c1658d3a6390ed23f9643df4479ee91e
suzannekbach <skbach@uw.edu>
2020-06-05 16:51:28 -0700

    :wrench: update config files for deploy

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+require('firebase/app');
+require('firebase/auth');
+require('firebase/firestore');
